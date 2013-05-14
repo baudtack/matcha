0a7dc4a925f6d622ff4eedc0821820a732db7777
David Kerschner <baudtack@baudtack.com>
2013-05-13 21:24:26 -0700

    added LICENSE

--- repl.js
+++ repl.js
@@ -1,7 +1,23 @@
+/*  matcha lisp - a lisp implementation in javascript
+    Copyright (C) 2013 David Kerschner
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 function Symbol(s) {
     this.s = s;
 }
-
 
 function tokenize(s) {
     return s.replace(/\(/g, ' ( ').replace(/\)/g, ' )').split(' ').filter(function (item) {
@@ -43,4 +59,3 @@ function parse(tokens, tree) {
 function read(s) {
     return parse(tokenize(s), []);
 }
-

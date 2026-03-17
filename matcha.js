427ea20c6086b84875af8d0b0909809b77dc32de
David Kerschner <david.kerschner@centralia.edu>
2026-03-17 11:42:49 -0700

    updated copyright year

--- matcha.js
+++ matcha.js
@@ -1,5 +1,5 @@
 /*  matcha lisp - a lisp implementation in javascript
-    Copyright (C) 2013 David Kerschner
+    Copyright (C) 2013-2026 David Kerschner
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
